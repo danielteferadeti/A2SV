dabe1b3dd932e6385234936de2430ae12cfeb1c4
Daniel Tefera Deti <danieltefera2211@gmail.com>
2021-01-10 21:42:32 +0300

    Sales by Match Uploaded

---
+++ Lab Practice/Week One/Day 2/SalesbyMatch.java
@@ -0,0 +1,71 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the sockMerchant function below.
+    static int sockMerchant(int n, int[] ar) {
+        int[] countedArray = new int[ar.length];
+        int pairCount = 0;
+        int totalPair = 0;
+        boolean checked = false;
+        
+        for(int i=0; i < ar.length; i++){
+            int current = ar[i];
+            for(int j=0; j < countedArray.length; j++){
+                if(current == countedArray[j]){
+                    checked = true; 
+                    break;
+                }
+            }
+            if(!checked){
+                for(int k=i; k < ar.length;k++){
+                    if(current == ar[k]){
+                        pairCount +=1;
+                    }
+                    if(pairCount==2){
+                        totalPair += 1;
+                        pairCount = 0;
+                    }
+                }
+            }
+            pairCount = 0;
+            checked = false;
+            countedArray[i]=current;
+        }
+        return totalPair;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
