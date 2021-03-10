df679a0fe31c29282f470716a1b8fb6cd48dca88
Daniel Tefera Deti <danieltefera2211@gmail.com>
2021-03-10 09:42:52 +0300

    uniqueOccurrences Solved

---
+++ Contest/Camping/Unique Number_of_Occurrences.py
@@ -0,0 +1,18 @@
+class Solution:
+    def uniqueOccurrences(self, arr: List[int]) -> bool:
+        visited = []
+        occCount = []
+        counter = 0
+        for i in range(len(arr)):
+            if arr[i] not in visited:
+                visited.append(arr[i])
+                for j in range(len(arr)):
+                    if arr[i] == arr[j]:
+                        counter +=1
+                if counter not in occCount and counter != 0:
+                    occCount.append(counter)
+                    print(str(occCount))
+                    counter = 0
+                else:
+                    return False       
+        return True
