1760b3d237577fdc9d05a898c35e460aef268c70
Daniel Tefera Deti <danieltefera2211@gmail.com>
2021-03-11 04:48:04 +0300

    .

--- Contest/Camping/Day 2/validate_stack_sequence.py
+++ Contest/Camping/Day 2/validate_stack_sequence.py
@@ -1,19 +1,21 @@
-class Solution:
-    def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
-        if not len(pushed) and not len(popped):
-            return False
+def validateStackSequences(self, pushed: List[int], popped: List[int]) -> bool:
+        if len(pushed) == 0 or len(popped) == 0:
+            return True
         startIndex = pushed.index(popped[0])
         stack = []
         
         for i in range(startIndex+1):
             stack.append(pushed[i])
+            
         for i in range(len(popped)):
             if popped[i] not in  stack:
-                print("Not in Stack")
+                if popped[i] != pushed[startIndex]:
+                    return False
                 stack.append(popped[i])
             if popped[i] == stack[-1]:
                 stack.pop()
-                #same how removed the popped last element too
+                startIndex += 1
             else:
                 return False
+            
         return True
