8dc8d65670eb44a3d4cfb70801ee335eb4144d9b
Daniel Tefera Deti <danieltefera2211@gmail.com>
2021-03-17 07:54:02 +0300

    Factorial Commited

---
+++ Lab Practice/After Day 7/Factorial.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the factorial function below.
+def fact(n):
+    if n == 1:
+        return 1
+    else:
+        return n * fact(n - 1)
+def factorial(n):
+    return fact(n)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    result = factorial(n)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
